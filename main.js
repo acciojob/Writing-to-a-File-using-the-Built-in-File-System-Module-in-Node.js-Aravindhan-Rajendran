e7545e754fe98b1f7986df05e859df3910a87499
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-18 11:32:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
 
-// TODO: Write the message "Hello, World!" to the file "output.txt"
+// Define the message to be written
+const message = "Hello, World!";
 
+// Use the writeFile function to write the message to output.txt
+fs.writeFile('output.txt', message, (err) => {
+    if (err) {
+        // Log the error if one occurs
+        console.error('Error writing to file:', err);
+        throw err;
+    }
+    console.log('Message written successfully!');
+});
